bbe7d5281b270371ef72b3bc1afad4a5f3be367e
Tech-Avrach <nikhilpatel9226@gmail.com>
2024-11-17 19:22:02 +0530

    comment added

--- decimalToBinary.cpp
+++ decimalToBinary.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+/**
+ * Converts a decimal number to its binary representation.
+ * 
+ * The function takes a single decimal number as argument and
+ * returns its binary representation as an integer.
+ * 
+ * For example, decToBinary(50) returns 110010.
+ * 
+ * @param decNum The decimal number to convert.
+ * @return The binary representation of the number as an integer.
+ */
 int decToBinary(int decNum) {
     int ans = 0, pow = 1;
 
